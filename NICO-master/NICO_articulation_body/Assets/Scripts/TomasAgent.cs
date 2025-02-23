670f6553e697f538415bd3fdfa65a0872557afde
Tomas Belak <108904119+tomasbelak24@users.noreply.github.com>
2025-02-23 22:24:06 +0100

    Update TomasAgent.cs

--- NICO-master/NICO_articulation_body/Assets/Scripts/TomasAgent.cs
+++ NICO-master/NICO_articulation_body/Assets/Scripts/TomasAgent.cs
@@ -7,57 +7,85 @@ using Unity.MLAgents.Sensors;
 
 public class TomasAgent : Agent
 {
-    public ArticulationBody NICO;
-    private ArticulationBody shoulder;
-    private ArticulationBody collarbone;
-    private ArticulationBody upperarm;
-    private ArticulationBody lowerarm;
-    private ArticulationBody forearm;
+    
+    ArticulationBody nico;
+
+    private List<float> initial_targets = new List<float>();
+    private List<float> initial_positions = new List<float>();
+    private List<float> initial_velocities = new List<float>();
+    private List<float> targets = new List<float>();
+    private List<float> initial_changes = new List<float>();
+    private List<float> changes = new List<float>();
+
+    private List<int> dof_ind = new List<int>();
+
+    private List<float> low_limits;
+    private List<float> high_limits;
+
+    private int dofs; // pocet dof
+    private int abs; // pocet articulation bodies
+
+
 
     [Tooltip("The target object")]
     public GameObject target;
     Vector3 defaultTargetPosition;
-    
+
     [Tooltip("End effector")]
-    public GameObject indexFingertip;
+    public GameObject effector;
 
-    private List<float> lowerLimits;
-    private List<float> upperLimits;
-    private List<ArticulationBody> joints;
+    private float last_dist;
 
-    private void get_limits()
+    
+    private void GetLimits(ArticulationBody root, List<float> llimits, List<float> hlimits)
     {
-    joints = new List<ArticulationBody>();
-    lowerLimits = new List<float>();
-    upperLimits = new List<float>();
-
-    joints.Add(shoulder);
-    joints.Add(collarbone);
-    joints.Add(upperarm);
-    joints.Add(lowerarm);
-    joints.Add(forearm);
-
-    foreach (var joint in joints)
-    {
-        if (joint != null)
+        GameObject curr_obj = root.gameObject;
+        int num_ch = curr_obj.transform.childCount;
+        for (int i = 0; i < num_ch; ++i)
         {
-            var drive = joint.xDrive;
-            lowerLimits.Add(drive.lowerLimit * Mathf.Deg2Rad);
-            upperLimits.Add(drive.upperLimit * Mathf.Deg2Rad);
+            // get articulation body component from child with index i, get its drive limits, write them to lists, call recursively on children
+            GameObject child = curr_obj.transform.GetChild(i).gameObject;
+            ArticulationBody child_ab = child.GetComponent<ArticulationBody>();
+            if (child_ab != null)
+            {
+                int j = child_ab.index;
+                llimits[j - 1] = child_ab.xDrive.lowerLimit;
+                hlimits[j - 1] = child_ab.xDrive.upperLimit;
+                GetLimits(child_ab, llimits, hlimits);
+            }
         }
     }
-}
 
 
     public override void Initialize()
     {
-        NICO = GetComponent<ArticulationBody>();
-        shoulder = NICO.transform.Find("r_shoulder").GetComponent<ArticulationBody>();
-        collarbone = NICO.transform.Find("r_collarbone").GetComponent<ArticulationBody>();
-        upperarm = NICO.transform.Find("r_upper_arm").GetComponent<ArticulationBody>();
-        lowerarm = NICO.transform.Find("r_lower_arm").GetComponent<ArticulationBody>();
-        forearm = NICO.transform.Find("r_forearm").GetComponent<ArticulationBody>();
-        get_limits();
+        base.Initialize();
+
+        // remember initial joint positions and velocities so we can reset them at next episode start
+
+        nico = GetComponent<ArticulationBody>();
+        nico.GetDofStartIndices(dof_ind);
+        abs = nico.GetDriveTargets(initial_targets);
+        nico.GetJointPositions(initial_positions);
+        nico.GetJointVelocities(initial_velocities);
+
+        low_limits = new List<float>(new float[abs]);
+        high_limits = new List<float>(new float[abs]);
+
+        GetLimits(nico, low_limits, high_limits);
+
+        defaultTargetPosition = target.transform.position;
+
+        for (int i = 0; i < dofs; ++i)
+        {
+            initial_changes.Add(0f);
+        }
+
+        changes = new List<float>(initial_changes);
+        targets = new List<float>(initial_targets);
+
+
+        last_dist = (target.transform.position - effector.transform.position).magnitude;
 
     
     }
@@ -66,21 +94,26 @@ public class TomasAgent : Agent
     
     public override void OnEpisodeBegin()
     {
-        target.transform.position = defaultTargetPosition + new Vector3(
-            Random.Range(-0.1f, 1.25f), 
-            Random.Range(0.0f, 0.05f), 
-            Random.Range(-0.2f, 0.2f)
-        );
+        target.transform.position = defaultTargetPosition + new Vector3(Random.Range(-0.1f, 0.95f), Random.Range(0.0f, 0.45f), Random.Range(-0.6f, 0.1f));
         
         target.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
 
-        //prevDistance = (indexFingertip.transform.position - target.transform.position).magnitude;
+        nico.SetDriveTargets(initial_targets);
+        nico.SetJointPositions(initial_positions);
+        nico.SetJointVelocities(initial_velocities);
+
+        changes = new List<float>(initial_changes);
+        targets = new List<float>(initial_targets);
     }
 
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        
+        // observe current targets
+        List<float> observation = new List<float>();
+        nico.GetDriveTargets(observation);
+        sensor.AddObservation(observation);
+        sensor.AddObservation(target.transform.position - effector.transform.position);
     }
 
 
@@ -89,14 +122,58 @@ public class TomasAgent : Agent
         float max_range = Mathf.Deg2Rad * 0.1f;
         float change_magnitude = Mathf.Deg2Rad * 0.05f;
 
-        
+        for (int i = 0; i < dofs; ++i)
+        {
+            changes[i] = Mathf.Clamp(changes[i] + actions.ContinuousActions[i] * change_magnitude, -max_range, max_range);
+        }
 
+        for (int i = 0; i < abs; ++i)
+        {
+                targets[i] = Mathf.Clamp(targets[i] + changes[i], Mathf.Deg2Rad * low_limits[i], Mathf.Deg2Rad * high_limits[i]);
+        }
+
+        // set joint targets to modified values
+
+        nico.SetDriveTargets(targets);
+
+        // calculate reward
+
+        float new_dist = (target.transform.position - effector.transform.position).magnitude;
+
+        // penalize for movements so the arm does not shake
+
+        float movement_reward = 0f;
+        for (int i = 0; i < dofs; ++i)
+        {
+            movement_reward += -0.5f * Mathf.Abs(changes[i]);
+        }
+
+        // give reward for moving closer to target
+
+        float got_closer_reward = (last_dist - new_dist) * 0.1f;
+
+        // penalize from moving away from target
+
+        if (got_closer_reward <= 0)
+        {
+            got_closer_reward = -3f;
+        }
+        last_dist = new_dist;
+
+        // penalize for distance to target
+
+        float proximity_reward = -1f * new_dist;
+
+        // add all reward components together and provide reward to agent
+
+        AddReward(got_closer_reward + proximity_reward + movement_reward);
     }
 
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-    
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
     }
         
 }
